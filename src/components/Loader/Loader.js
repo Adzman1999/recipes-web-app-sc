01eebe179c84b9bea50f47b3a1f71dcdf6fc7b04
Mhan1999 <abdulsamadadzmani@gmail.com>
2023-09-01 18:11:12 +0800

    first commit

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.scss";
+
+const Loader = () => {
+  return (
+    <div className='loader my-5'>
+      <div className='container flex align-center justify-center'></div>
+    </div>
+  );
+};
+
+export default Loader;
